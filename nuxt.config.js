5feac16c125c43bfa0238aa9151660882a1ba1e8
jdtzmn <jdtzmn@gmail.com>
2018-09-25 11:51:56 -0400

    Added favicon.ico and title (#5)

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,11 +3,13 @@ import StylelintPlugin from 'stylelint-webpack-plugin'
 
 module.exports = {
   head: {
+    title: 'Jacob Daitzman',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' }
     ],
     link: [
+      { rel: 'icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto+Slab' }
     ]
   },
