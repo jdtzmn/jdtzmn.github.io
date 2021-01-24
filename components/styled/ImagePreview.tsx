5cb2941054ab9b9e0b49f454bfaf5e60d3083e99
Jacob Daitzman <jdtzmn@gmail.com>
2021-01-24 18:08:49 -0500

    Blurry-load images like Medium

--- components/styled/ImagePreview.tsx
+++ components/styled/ImagePreview.tsx
@@ -1,24 +1,47 @@
-import Image, { ImageLoaderProps } from 'next/image'
+import { useState, useEffect, useRef } from 'react'
+// eslint-disable-next-line import/no-named-default
+import { default as NextImage, ImageLoaderProps, ImageProps } from 'next/image'
+import { useIntersection } from 'next/dist/client/use-intersection'
 import styled from 'styled-components'
-import { addHttpsIfNecessary } from 'src/utils'
+import { addHttpsIfNecessary, mergeRefs } from 'src/utils'
 
 const defaultImageQuality = 75
+const lowQualityImageQuery = 'w=11&h=8'
 
 function contentfulLoader({ src, width, quality }: ImageLoaderProps) {
-  return `${src}?w=${width}&q=${quality || defaultImageQuality}`
+  // Guard against gif, since resizing a gif takes more time than it saves
+  if (src.includes('.gif')) {
+    return src
+  }
+
+  // Customize image width and quality if not customized already
+  const srcUrl = new URL(src)
+  if (!srcUrl.searchParams.has('w')) {
+    srcUrl.searchParams.set('w', width.toString())
+  }
+  if (!srcUrl.searchParams.has('q')) {
+    srcUrl.searchParams.set('q', (quality || defaultImageQuality).toString())
+  }
+
+  // Use webp when available
+  if (!srcUrl.searchParams.has('fm')) {
+    srcUrl.searchParams.set('fm', 'webp')
+  }
+
+  return srcUrl.toString()
 }
 
 interface StyledNextImageProps {
-  styleImage?: boolean
+  $styleImage?: boolean
 }
 
-const StyledNextImage = styled(Image).attrs(({ src }) => ({
+export const StyledNextImage = styled(NextImage).attrs(({ src }) => ({
   loader: contentfulLoader,
   layout: 'responsive',
-  src: addHttpsIfNecessary(src), // fix `//domain.com` urls
+  src: addHttpsIfNecessary(src), // fix `//domain.com` urls, where `https` is missing
 }))<StyledNextImageProps>`
-  ${({ styleImage, theme }) =>
-    styleImage
+  ${({ $styleImage, theme }) =>
+    $styleImage
       ? `
         border-radius: 16px;
         box-shadow: 0 5px 30px ${theme.colors.black};
@@ -26,10 +49,98 @@ const StyledNextImage = styled(Image).attrs(({ src }) => ({
       : ''}
 `
 
+interface LoadingContainerProps {
+  loaded: boolean
+}
+
+const LoadingContainer = styled.div<LoadingContainerProps>`
+  position: relative;
+  transition: filter 0.3s;
+  ${({ loaded }) => !loaded && 'filter: blur(10px);'}
+`
+
+const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+`
+
+type LoadingImageProps = Omit<ImageProps & StyledNextImageProps, 'layout'>
+
+export function LoadingImage(props: LoadingImageProps) {
+  const imgParentRef = useRef<HTMLDivElement>()
+
+  const [loaded, setLoaded] = useState(false)
+
+  // Use the intersection hook that next.js uses for image detection
+  const [setRef, isIntersected] = useIntersection<HTMLDivElement>({
+    rootMargin: '200px',
+    disabled: props.priority,
+  })
+
+  function handleImageLoad(img: HTMLImageElement) {
+    // Mark the image as loaded
+    setLoaded(true)
+
+    // Delete the image element
+    img.remove()
+  }
+
+  useEffect(() => {
+    // Only load images once intersected
+    if (isIntersected || props.priority) {
+      // Get the image src of the image element that is being loaded
+      const imgParentEl = imgParentRef.current
+
+      // Short circuit if the parent element is undefined
+      if (typeof imgParentEl === 'undefined') return
+      const imgEl = imgParentEl.querySelector('img')
+
+      // Create an image element to track the loading
+      const img = new Image()
+      img.addEventListener('load', () => handleImageLoad(img), false)
+      img.src = imgEl.src
+    }
+  }, [isIntersected])
+
+  const lowQualityImage = `${props.src}?${lowQualityImageQuery}`
+
+  return (
+    <LoadingContainer loaded={loaded} ref={mergeRefs(setRef, imgParentRef)}>
+      <StyledNextImage {...props} />
+      {!loaded && (
+        <Overlay>
+          <StyledNextImage {...props} priority src={lowQualityImage} />
+        </Overlay>
+      )}
+    </LoadingContainer>
+  )
+}
+
+interface ImagePreviewProps {
+  loadIn?: boolean
+}
+
+export default function ImagePreview(
+  props: LoadingImageProps & ImagePreviewProps
+) {
+  // By default, load in if `priority` prop is not set
+  const loadInSet = typeof props.loadIn === 'boolean'
+  const shouldLoadIn = !loadInSet || props.loadIn
+
+  // Load in the image
+  if (shouldLoadIn) {
+    return <LoadingImage {...props} />
+    // Or don't if `loadIn` is false
+  } else {
+    return <StyledNextImage {...props} />
+  }
+}
+
 export const ImagePreviewContainer = styled.div`
   & > div {
     overflow: visible !important;
   }
 `
-
-export default StyledNextImage
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,5 @@
+import { Ref, MutableRefObject, RefObject } from 'react'
+
 // ensure that an environment variable is set
 export function guardEnv(name: string, value?: string) {
   const valueIsUndefined = typeof value === 'undefined'
@@ -15,6 +17,42 @@ export function guardEnv(name: string, value?: string) {
  * @returns {string} The url string prepended with `https:` if needed
  */
 export function addHttpsIfNecessary(url: string): string {
-  if (!url.includes('https:') && url.slice(0, 2) === '//') return `https:${url}`
+  if (
+    typeof url === 'string' &&
+    !url.includes('https:') &&
+    url.slice(0, 2) === '//'
+  ) {
+    return `https:${url}`
+  }
+
   return url
+}
+
+/**
+ * Merge a set of React refs into one ref to pass to the component
+ *
+ * @export
+ * @param {...Array<Ref<any>>} refs A set of refs
+ * @return {Ref<any>} A new "merged" ref
+ */
+export function mergeRefs(...refs: Array<Ref<any>>): Ref<any> {
+  const filteredRefs = refs.filter(Boolean)
+
+  switch (filteredRefs.length) {
+    case 0:
+      return null
+    case 1:
+      return filteredRefs[0]
+    default:
+  }
+
+  return (inst: any) => {
+    for (const ref of filteredRefs) {
+      if (typeof ref === 'function') {
+        ref(inst)
+      } else if (ref) {
+        ;(ref as MutableRefObject<any>).current = inst
+      }
+    }
+  }
 }
