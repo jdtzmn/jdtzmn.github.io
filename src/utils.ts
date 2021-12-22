ed197e17a8058acc93b347aa456a7b2a51f4536e
Jacob Daitzman <jdtzmn@gmail.com>
2021-12-22 00:18:54 -0500

    Use custom env error with the `guardEnv` utility method

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,17 @@
 import { Ref, MutableRefObject } from 'react'
 
 // ensure that an environment variable is set
+class EnvironmentError extends Error {
+  constructor(message: string) {
+    super(message)
+    this.name = 'EnvironmentError'
+  }
+}
+
 export function guardEnv(name: string, value?: string) {
   const valueIsUndefined = typeof value === 'undefined'
   if (valueIsUndefined && typeof process.env[name] === 'undefined') {
-    throw new Error(`${name} environment variable must be set.`)
+    throw new EnvironmentError(`${name} environment variable must be set.`)
   }
 
   return valueIsUndefined ? process.env[name] : value
