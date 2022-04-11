bee967a80f3de4b23f1c8dac7a5bb07fc13623e5
Jacob Daitzman <jdtzmn@gmail.com>
2022-04-11 15:29:13 -0400

    Switch from sendgrid to nodemailer
    * Remove hcaptcha dependency in non-production environments

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,11 +1,27 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import sgMail from '@sendgrid/mail'
+import nodemailer, { SendMailOptions } from 'nodemailer'
 import { stripHtml } from 'string-strip-html'
 import axios from 'axios'
 import { withSentry } from '@sentry/nextjs'
 import { guardEnv } from 'src/utils'
 
-sgMail.setApiKey(guardEnv('SENDGRID_API_KEY'))
+// Cache transporter
+let transporter: nodemailer.Transporter
+function getTransporter() {
+  if (!transporter) {
+    transporter = nodemailer.createTransport({
+      host: guardEnv('CONTACT_SMTP_HOST'),
+      port: Number(guardEnv('CONTACT_SMTP_PORT')),
+      secure: true,
+      auth: {
+        user: guardEnv('CONTACT_SMTP_USER'),
+        pass: guardEnv('CONTACT_SMTP_PASS'),
+      },
+    })
+  }
+
+  return transporter
+}
 
 function emailContent(message: string, from: string) {
   const html = `
@@ -36,17 +52,17 @@ function constructEmail(
 ) {
   const [html, text] = emailContent(message, from)
 
-  const msg: sgMail.MailDataRequired = {
+  const msg: SendMailOptions = {
     to: guardEnv('NEXT_PUBLIC_CONTACT_EMAIL'),
     from: {
-      email: guardEnv('SENDGRID_SENDER'),
-      name,
+      address: guardEnv('CONTACT_SMTP_USER'),
+      name: 'Portfolio Website Contact Form',
     },
     subject: `New message from ${name}`,
     text,
     html,
     replyTo: {
-      email,
+      address: email,
       name,
     },
   }
@@ -54,22 +70,13 @@ function constructEmail(
   return msg
 }
 
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method !== 'POST') return res.status(404).end()
-  const { name, email, message, captcha, from } = req.body
-
-  // guards
-  const invalidName = typeof name !== 'string' || name === ''
-  const invalidEmail =
-    typeof email !== 'string' ||
-    email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i) === null
-  const invalidMessage = typeof message !== 'string' || message.length > 1500
-  const invalidCaptcha = typeof captcha !== 'string'
-  if (invalidName || invalidEmail || invalidMessage || invalidCaptcha) {
-    return res.status(400).end()
-  }
-
-  // verify captcha
+/**
+ * Check whether a captcha is valid
+ * @returns True if valid or the response otherwise
+ */
+async function isCaptchaValid(
+  captcha: string
+): Promise<[isValid: boolean, errorResponse?: any]> {
   const verifyUrl = 'https://hcaptcha.com/siteverify'
   const requestBody = {
     secret: guardEnv('HCAPTCHA_SECRET_KEY'),
@@ -87,14 +94,49 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     new URLSearchParams(requestBody).toString(),
     config
   )
-  if (!response.data.success) {
-    console.error(response.data)
+
+  if (response.data.success) {
+    return [true, undefined]
+  } else {
+    return [false, response.data]
+  }
+}
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method !== 'POST') return res.status(404).end()
+  const { name, email, message, captcha, from } = req.body
+
+  // guards against common form fields
+  const invalidName = typeof name !== 'string' || name === ''
+  const invalidEmail =
+    typeof email !== 'string' ||
+    email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i) === null
+  const invalidMessage = typeof message !== 'string' || message.length > 1500
+  if (invalidName || invalidEmail || invalidMessage) {
     return res.status(400).end()
+  }
+
+  // verify captcha, if in a production environment
+  const isProduction = process.env.NODE_ENV === 'production'
+  if (isProduction) {
+    // confirm it is present
+    const invalidCaptcha = typeof captcha !== 'string'
+    if (invalidCaptcha) {
+      return res.status(400).end()
+    }
+
+    // verify it is valid
+    const [captchaIsValid, errorResponse] = await isCaptchaValid(captcha)
+
+    if (!captchaIsValid) {
+      console.error(errorResponse)
+      return res.status(400).end()
+    }
   }
 
   // send email
   const msg = constructEmail(name, email, message, from)
-  await sgMail.send(msg)
+  await getTransporter().sendMail(msg)
   res.end()
 }
 
